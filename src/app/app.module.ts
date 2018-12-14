64a61c0874adeebd21cc0b273f8268050941c362
NAVIN V <navin.vinayagam@syncfusion.com>
2018-12-14 16:03:51 +0530

    Comment Line Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//Imported DateRangePicker module from ej2-angular-calendars package
 import { DateRangePickerModule } from '@syncfusion/ej2-angular-calendars';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, DateRangePickerModule
+    BrowserModule, 
+    //Registering DateRangePicker Module 
+    DateRangePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
